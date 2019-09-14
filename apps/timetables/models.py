dca22e78d01af73ef9416e598c08620c0f090060
Kazuki Imamura <kaizumaki@gmail.com>
2019-09-14 17:19:55 +0900

    Add models of timetables

--- apps/timetables/models.py
+++ apps/timetables/models.py
@@ -1,3 +1,65 @@
 from django.db import models
 
-# Create your models here.
+
+class TimeTable(models.Model):
+    id = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=200)
+    is_published = models.BooleanField(default=False)
+    slug = models.CharField(max_length=200, null=True, blank=True)
+    created_date = models.DateTimeField(auto_now_add=True)
+    modified_date = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+
+class TimeTableRow(models.Model):
+    id = models.AutoField(primary_key=True)
+    table_id = models.ForeignKey(TimeTable, on_delete=models.CASCADE)
+    start_at = models.DateTimeField(null=True, blank=True)
+    duration = models.IntegerField(null=True, blank=True)
+    created_date = models.DateTimeField(auto_now_add=True)
+    modified_date = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.id
+
+
+class TimeTableColumn(models.Model):
+    id = models.AutoField(primary_key=True)
+    table_id = models.ForeignKey(TimeTable, on_delete=models.CASCADE)
+    name = models.CharField(max_length=50, null=True, blank=True)
+    created_date = models.DateTimeField(auto_now_add=True)
+    modified_date = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.id
+
+
+class Talk(models.Model):
+    id = models.AutoField(primary_key=True)
+    speaker_name = models.CharField(max_length=100)
+    title = models.CharField(max_length=200)
+    status = models.CharField(max_length=100)
+    tag = models.CharField(max_length=100)
+    talk_language = models.CharField(max_length=100)
+    slide_language = models.CharField(max_length=100)
+    target_audience = models.CharField(max_length=100)
+    created_date = models.DateTimeField(auto_now_add=True)
+    modified_date = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+
+class TimeTableItem(models.Model):
+    id = models.AutoField(primary_key=True)
+    row_id = models.ForeignKey(TimeTableRow, on_delete=models.CASCADE)
+    column_id = models.ForeignKey(TimeTableColumn, on_delete=models.CASCADE)
+    talk_id = models.ForeignKey(Talk, on_delete=models.CASCADE)
+    is_empty = models.BooleanField(default=False)
+    created_date = models.DateTimeField(auto_now_add=True)
+    modified_date = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.id
