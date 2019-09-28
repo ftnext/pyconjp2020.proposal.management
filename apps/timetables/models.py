46523c903690680221bd107116881855f6768d36
ftnext <takuyafjp+develop@gmail.com>
2019-09-28 13:50:45 +0900

    [feat] TimeTableモデルのインスタンスをソートするためにcreated_dateプロパティを導入（uuidではソートできないことに気づいた）

--- apps/timetables/models.py
+++ apps/timetables/models.py
@@ -1,6 +1,7 @@
 import uuid
 
 from django.db import models
+from django.utils import timezone
 
 
 class TimeTable(models.Model):
@@ -8,6 +9,7 @@ class TimeTable(models.Model):
     title = models.CharField("タイトル", max_length=100)
     description = models.TextField("詳細情報", blank=True, null=True)
     is_published = models.BooleanField("タイムテーブルの公開", default=False)
+    created_date = models.DateTimeField(default=timezone.now)
 
     def __str__(self):
         return self.title
