ef9f355b5b4b0652cd9dd1d4036e84669345da47
ftnext <takuyafjp+develop@gmail.com>
2019-09-28 12:09:21 +0900

    [feat] TimeTableモデルを追加
    * uuidを試した（タイムテーブル詳細画面のURLに使う想定）
    * 詳細情報を追加するdescriptionという項目を追加した

--- apps/timetables/models.py
+++ apps/timetables/models.py
@@ -1,3 +1,13 @@
+import uuid
+
 from django.db import models
 
-# Create your models here.
+
+class TimeTable(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    title = models.CharField("タイトル", max_length=100)
+    description = models.TextField("詳細情報", blank=True, null=True)
+    is_published = models.BooleanField("タイムテーブルの公開", default=False)
+
+    def __str__(self):
+        return self.title
