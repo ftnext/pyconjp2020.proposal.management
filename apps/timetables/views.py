126e6f94296f11e691c8a0111e8f1fa3b0d8270a
ftnext <takuyafjp+develop@gmail.com>
2019-09-28 13:51:42 +0900

    [feat] タイムテーブルの一覧表示機能を追加（shellで作ったレコードを表示）

--- apps/timetables/views.py
+++ apps/timetables/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
 
 from .forms import TimeTableForm
+from .models import TimeTable
 
 
 def table_list(request):
+    timetables = TimeTable.objects.order_by("created_date")
     form = TimeTableForm()
-    return render(request, "timetables/table_list.html", {"form": form})
+    return render(
+        request,
+        "timetables/table_list.html",
+        {"form": form, "timetables": timetables},
+    )
